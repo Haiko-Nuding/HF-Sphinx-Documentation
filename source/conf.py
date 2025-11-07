7b6f5681f1d26792f0714b03fac7a53353a4b5c8
Haiko <haikonudingmorales@yahoo.com>
2025-11-07 17:24:33 +0100

    [init] sphinx-doc: initialize sphinx-documentation.

---
+++ source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'HF-Sphinx-Documentation'
+copyright = '2025, Haiko Nuding'
+author = 'Haiko Nuding'
+
+# These are options specifically for the Wagtail Theme.
+html_theme_options = dict(
+    project_name = "HF-Sphinx-Documentation",
+    logo_alt = "Wagtail",
+    logo_height = 59,
+    logo_url = "/",
+    logo_width = 45,
+)
+
+html_theme_options = dict(
+    github_url = "https://github.com/Haiko-Nuding/HF-Sphinx-Documentation"
+)
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_wagtail_theme']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_wagtail_theme'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,17 @@
+.. HF-Sphinx-Documentation documentation master file, created by
+   sphinx-quickstart on Fri Nov  7 17:15:57 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+HF-Sphinx-Documentation documentation
+=====================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
